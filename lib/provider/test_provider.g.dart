de7708aa971d1a2a31b8bc01c7d057401c23f70f
plashdof <plashdof@gmail.com>
2025-03-17 00:31:42 +0900

    feat: create test provider

---
+++ lib/provider/test_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'test_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$testProviderHash() => r'3a807cae404da1659246038ffe70a59d3f80ad3f';
+
+/// See also [TestProvider].
+@ProviderFor(TestProvider)
+final testProviderProvider =
+    AutoDisposeNotifierProvider<TestProvider, String>.internal(
+      TestProvider.new,
+      name: r'testProviderProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$testProviderHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$TestProvider = AutoDisposeNotifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
