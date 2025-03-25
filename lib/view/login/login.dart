23247bef02a626c6b91e86ac6a2d523b3c41508b
plashdof <plashdof@gmail.com>
2025-03-26 01:26:05 +0900

    feat: google login setting

--- lib/view/login/login.dart
+++ lib/view/login/login.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lugeasy/view/main/main_container.dart';
 
-import '../../services/intro_services.dart';
 import '../../provider/apple_auth_provider.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginPage extends ConsumerWidget {
   const LoginPage({super.key});
@@ -29,24 +30,22 @@ class LoginPage extends ConsumerWidget {
               if (Platform.isIOS) ...[
                 // iOS
                 ElevatedButton(
-                  onPressed:
-                      appleLoginState is AsyncLoading
-                          ? null
-                          : () => _handleLogin(context, ref, "Apple Login"),
-                  child:
-                      appleLoginState is AsyncLoading
-                          ? CircularProgressIndicator()
-                          : Text("Apple Login"),
+                  onPressed: appleLoginState is AsyncLoading
+                      ? null
+                      : () => _appleLogin(context, ref),
+                  child: appleLoginState is AsyncLoading
+                      ? CircularProgressIndicator()
+                      : Text("Apple Login"),
                 ),
                 SizedBox(height: 10),
                 ElevatedButton(
-                  onPressed: () => _handleLogin(context, ref, "Google Login"),
+                  onPressed: () => _googleLogin(context, ref),
                   child: Text("Google Login"),
                 ),
               ] else if (Platform.isAndroid) ...[
                 // Android
                 ElevatedButton(
-                  onPressed: () => _handleLogin(context, ref, "Google Login"),
+                  onPressed: () => _googleLogin(context, ref),
                   child: Text("Google Login"),
                 ),
               ],
@@ -57,50 +56,53 @@ class LoginPage extends ConsumerWidget {
     );
   }
 
-  Future<void> _handleLogin(
-    BuildContext context,
-    WidgetRef ref,
-    String loginType,
-  ) async {
+  Future<void> _appleLogin(BuildContext context, WidgetRef ref) async {
     try {
-      final introServices = IntroServices();
+      final appleNotifier = ref.read(appleAuthNotifierProvider.notifier);
+      await appleNotifier.signInWithApple();
 
-      if (loginType == "Apple Login") {
-        // Apple 로그인 처리
-        final appleNotifier = ref.read(appleAuthNotifierProvider.notifier);
-        await appleNotifier.signInWithApple();
-
-        final appleLoginState = ref.read(appleAuthNotifierProvider);
-        appleLoginState.when(
-          data: (token) {
-            if (token != null) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => MainContainer()),
-              );
-            }
-          },
-          error: (error, _) {
-            ScaffoldMessenger.of(
+      final appleLoginState = ref.read(appleAuthNotifierProvider);
+      appleLoginState.when(
+        data: (token) {
+          if (token != null) {
+            Navigator.push(
               context,
-            ).showSnackBar(SnackBar(content: Text("Apple 로그인 실패: $error")));
-          },
-          loading: () {}, // 로딩 중 UI 처리는 UI에서 진행
-        );
-      } else if (loginType == "Google Login") {
-        // Google 로그인 처리
-        final response = await introServices.mockLoginApi(loginType);
-        if (response == "success") {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => MainContainer()),
-          );
-        } else {
+              MaterialPageRoute(builder: (context) => MainContainer()),
+            );
+          }
+        },
+        error: (error, _) {
           ScaffoldMessenger.of(
             context,
-          ).showSnackBar(SnackBar(content: Text("Login Failed: $response")));
-        }
-      }
+          ).showSnackBar(SnackBar(content: Text("Apple 로그인 실패: $error")));
+        },
+        loading: () {}, // 로딩 중 UI 처리는 UI에서 진행
+      );
+    } catch (error) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Error: ${error.toString()}")));
+    }
+  }
+
+  Future<void> _googleLogin(BuildContext context, WidgetRef ref) async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      final token = await FirebaseAuth.instance.signInWithCredential(
+        credential,
+      );
+      debugPrint(token.toString());
     } catch (error) {
       ScaffoldMessenger.of(
         context,
