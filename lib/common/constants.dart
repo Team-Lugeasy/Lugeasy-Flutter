ef4f300d8777c082be786b2856220fb014093ba5
YooJisu826 <youonl5@naver.com>
2025-04-09 17:00:47 +0900

    feat: temporary profile icon

---
+++ lib/common/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kDefaultProfileIcon = CircleAvatar(
+  backgroundColor: Color(0xFFDBDBDB),
+  radius: 52,
+  child: Icon(Icons.person, size: 52, color: Color(0xFFC5C5C5)),
+);
