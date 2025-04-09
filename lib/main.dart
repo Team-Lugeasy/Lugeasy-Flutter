4d786034fbd7cbc3c544a31a7072472c6c9a3972
plashdof <plashdof@gmail.com>
2025-04-09 15:37:38 +0900

    feat : create bottomnav

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lugeasy/view/login/login.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:lugeasy/view/main/main_container.dart';
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(ProviderScope(child: const MyApp()));
 }
 
@@ -16,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: LoginPage(),
+      home: MainContainer(),
     );
   }
 }
--- lib/view/main/main_container.dart
+++ lib/view/main/main_container.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lugeasy/view/main/map/map_page.dart';
+import 'package:lugeasy/view/main/matching/matching_page.dart';
+import 'package:lugeasy/view/main/mypage/my_page.dart';
 
 class MainContainer extends StatefulWidget {
   const MainContainer({super.key});
@@ -8,23 +11,32 @@ class MainContainer extends StatefulWidget {
 }
 
 class _MainContainerState extends State<MainContainer> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    MatchingPage(),
+    MapPage(),
+    MyPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Main Page",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 50),
-            ],
-          ),
-        ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Matching'),
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Map'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Mypage'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
